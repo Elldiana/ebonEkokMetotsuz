0511e944b6087272d8b65e22491dc7bcdacce10c
Eldiana <129501744+Elldiana@users.noreply.github.com>
2025-01-28 13:49:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EbobEkok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ebob=1;
+            Console.Write("1. sayı giriniz: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("2. sayi giriniz: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            if (sayi1 < sayi2)
+            {
+                for(int a =1; a<sayi2; a++)
+                {
+                    if(sayi2 % a ==0 && sayi1 % a == 0)
+                    {
+                        ebob = a;
+                    }
+                }
+            }
+            else
+            {
+                for (int a = 1; a < sayi1; a++)
+                {
+                    if (sayi2 % a == 0 && sayi1 % a == 0)
+                    {
+                        ebob = a;
+                    }
+                }
+            }
+            Console.WriteLine("EBOB {0}", ebob);
+            Console.WriteLine("EKOK {0}", (sayi1*sayi2)/ebob);
+            Console.ReadKey();
+        }
+    }
+}
